42ac9708db59e9715d2a8325b2bf95af0a313906
Abdurrazzak Shaikh <Abdurrazzak Shaikh@ho.rnt.ai>
2025-11-08 16:14:46 +0530

    i added an another llm model for 2. parallelwork flow folder 2_paralleization

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from agentic-ai-langgraph!")
+# students = [
+#     ("Alice", "Math", 85),
+#     ("Alice", "Science", 92),
+#     ("Bob", "Math", 78),
+#     ("Bob", "Science", 80),
+#     ("Charlie", "Math", 90),
+#     ("Charlie", "Science", 85)
+# ]
 
+# d = {}
 
-if __name__ == "__main__":
-    main()
+# for i in students:
+#     name = i[0]
+#     sub = i[1]
+#     marks = i[2]
+
+#     if name not in d:
+#         d[name] = {}
+    
+#     d[name][sub] = marks
+
+        
+# print(d)
+
+# ---------------------------------------------------------------------------------
+
+arr=[
+    {"fruit":"mango","quantity":10},
+    {"fruit":"apple","quantity":2},
+    {"fruit":"mango","quantity":10},
+    {"fruit":"mango","quantity":10},
+    {"fruit":"banana","quantity":10},
+    ]
+# OUTPUT:
+# [
+#   { fruit: 'mango', quantity: 30 },
+#   { fruit: 'apple', quantity: 2 },
+#   { fruit: 'banana', quantity: 10 }
+# ]
